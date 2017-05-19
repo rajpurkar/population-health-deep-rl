3e078bf5a5b22267271204a2df8c7a67946aee34
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-05-18 19:54:54 -0700

    Rm legacy script

--- dta-to-csv.r
+++
@@ -1,8 +0,0 @@
-library(foreign)
-infile <- 'NGIR6AFL.dta?dl=0'
-data <- read.dta(infile)
-data_labels <- attr(data,"var.labels")
-combined_column_names <- paste(colnames(data), data_labels, sep=" | ")
-colnames(data) <- combined_column_names
-data_outfile <- paste(strsplit(infile, '.dta')[[1]][1], ".csv", sep="")
-write.table(data, file=data_outfile, sep=",")
