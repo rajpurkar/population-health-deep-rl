Softmax fix, self.y hardcode dimensions

dual.py
@@ -115,7 +115,7 @@ class SimpleQN(DQN):
             if self.config.k_class:
                 state_shape = list(self.env.observation_space.shape)
                 k = state_shape[0] + 1
-                pred = layers.fully_connected(common, k, activation_fn=tf.softmax)
+                pred = layers.fully_connected(common, k, activation_fn=None)
             else:
                 pred = layers.fully_connected(common, 1, activation_fn=tf.sigmoid)
             out = layers.fully_connected(common, num_actions, activation_fn=None)

utils/replay_buffer.py
@@ -164,7 +164,7 @@ class ReplayBuffer(object):
             self.obs      = np.empty([self.size] + list(frame.shape), dtype=np.uint8)
             self.action   = np.empty([self.size],                     dtype=np.int32)
             self.reward   = np.empty([self.size],                     dtype=np.float32)
-            self.y        = np.empty([self.size],                     dtype=np.int32)
+            self.y        = np.empty([self.size, 6],                     dtype=np.int32)
             self.done     = np.empty([self.size],                     dtype=np.bool)
         self.obs[self.next_idx] = frame
 