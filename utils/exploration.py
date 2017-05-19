02720adebf6f2c9e526f9b2c773b541a8017dc13
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2017-05-19 13:22:47 -0700

    Add exploration in utils

---
+++ utils/exploration.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import random
+
+
+class LinearSchedule(object):
+    def __init__(self, eps_begin, eps_end, nsteps):
+        """
+        Args:
+            eps_begin: initial exploration
+            eps_end: end exploration
+            nsteps: number of steps between the two values of eps
+        """
+        self.epsilon = eps_begin
+        self.eps_begin = eps_begin
+        self.eps_end = eps_end
+        self.nsteps = nsteps
+
+
+    def update(self, t):
+        """
+        Updates epsilon
+
+        Args:
+            t: (int) nth frames
+        """
+        self.epsilon = max((((self.eps_end - self.eps_begin) * 1.0 / self.nsteps) * t) + self.eps_begin, self.eps_end)
+
+
+class LinearExploration(LinearSchedule):
+    def __init__(self, env, eps_begin, eps_end, nsteps):
+        """
+        Args:
+            env: gym environment
+            eps_begin: initial exploration
+            eps_end: end exploration
+            nsteps: number of steps between the two values of eps
+        """
+        self.env = env
+        super(LinearExploration, self).__init__(eps_begin, eps_end, nsteps)
+
+
+    def get_action(self, best_action):
+        """
+        Returns a random action with prob epsilon, otherwise return the best_action
+
+        Args:
+            best_action: (int) best action according some policy
+        Returns:
+            an action
+        """
+        if random.random() < self.epsilon:
+            action = self.env.action_space.sample()
+        else:
+            action = best_action
+        return action
