92fb6a31d994587b9e11dc190ff3757ca78a926e
Rajpurkar <pranav.rajpurkar@gmail.com>
2017-05-28 17:35:28 -0700

    Merge header and data processing

--- load/process.py
+++ load/process.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import random
 import pprint
+import shlex
 pp = pprint.PrettyPrinter(depth=6)
 
 
@@ -16,20 +17,46 @@ def get_random_line(afile):
     return line
 
 
-def process(filename):
-    with open(filename, 'r') as f:
+def get_single_record(data_file):
+    field_to_value_for_record = {}
+
+    with open(data_file, 'r') as f:
         first_line = f.readline().split(',')
         random_line = get_random_line(f).split(',')
 
-    d = {}
     for field, value in zip(first_line, random_line):
         if value != '':
-            d[field] = value
-    pp.pprint(d)
+            field_to_value_for_record[field] = value
+    return field_to_value_for_record
+
+
+def process(file, header_file):
+    field_to_value_for_record = get_single_record(file)
+    fields_to_names = get_field_names(header_file)
+    names_to_value = {}
+    for field in field_to_value_for_record:
+        try:
+            name = fields_to_names[field]
+            names_to_value[name] = field_to_value_for_record[field]
+        except:
+            continue
+    pp.pprint(names_to_value)
+
+
+def get_field_names(filename):
+    fields_to_names = {}
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = shlex.split(line)
+            if len(line) > 1 and line[1] == 'variable':
+                fields_to_names[line[2]] = line[3]
+    return fields_to_names
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process data.')
     parser.add_argument('file', help='CSV file to load')
+    parser.add_argument('header_file', help='header file to load')
     args = parser.parse_args()
-    process(args.file)
+    process(args.file, args.header_file)
