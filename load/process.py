4dedb93e53563530dcb847e112d2533a9c57e7a3
Rajpurkar <pranav.rajpurkar@gmail.com>
2017-05-30 15:30:42 -0700

    Replace process

--- load/process.py
+++ load/process.py
@@ -8,36 +8,14 @@ import pprint
 import shlex
 import collections
 import glob
+import csv
+from tqdm import tqdm
 pp = pprint.PrettyPrinter(depth=6)
 
-
-def get_random_line(afile):
-    line = next(afile)
-    for num, aline in enumerate(afile):
-      if random.randrange(num + 2): continue
-      line = aline
-    return line
-
-
-def get_single_record(data_file):
-    field_to_value_for_record = {}
-
-    with open(data_file, 'r') as f:
-        first_line = f.readline().split(',')
-        random_line = get_random_line(f).split(',')
-
-    for field, value in zip(first_line, random_line):
-        value = value.strip()
-        field = field.strip()
-        if value != '':
-            field_to_value_for_record[field] = value
-    return field_to_value_for_record
-
-
-def process_headers(filename):
+def process_metadata(filename):
     fields_to_names = {}
     fields_to_parent_fields = {}
-    field_to_values_to_interprets = collections.defaultdict(dict)
+    field_value_interpret = collections.defaultdict(dict)
     with open(filename, 'r') as f:
         lines = iter(f.readlines())
         while True:
@@ -56,11 +34,11 @@ def process_headers(filename):
                     if line is None or line == ';\r\n':
                         break
                     line = shlex.split(line)
-                    field_to_values_to_interprets[field][line[0]] = line[1]
+                    field_value_interpret[field][line[0]] = line[1]
     return (
         fields_to_names,
         fields_to_parent_fields,
-        field_to_values_to_interprets)
+        field_value_interpret)
 
 
 def process_types(dct_file):
@@ -72,6 +50,7 @@ def process_types(dct_file):
                 fields_to_types[line[1]] = line[0]
     return fields_to_types
 
+
 def get_parent_field(field, fields_to_parent_fields):
     if field in fields_to_parent_fields:
         parent_field = fields_to_parent_fields[field]
@@ -81,40 +60,76 @@ def get_parent_field(field, fields_to_parent_fields):
 
 
 def get_interpret_value(field, value, field_value_label):
+    if value == '': return value
     interpret_value = value
     if field in field_value_label:
-        value = str(int(float(value)))
+        try:
+            value = str(int(float(value)))
+        except:
+            value = value
         if (value in field_value_label[field]):
             interpret_value = field_value_label[field][value]
     return interpret_value
 
 
-def process(csv_file, do_file, dct_file, only_interpretable=False):
+def filter_headers(
+        headers,
+        fields_to_types,
+        fields_to_parent_fields):
+    new_headers = []
+    for field in headers:
+        assert(field == field.strip())
+        if field == '': continue
+        field_type = fields_to_types[field]
+        if (field_type != 'byte'): continue
+        parent_field = get_parent_field(
+            field, fields_to_parent_fields)
+        if (parent_field == field): continue
+        new_headers.append(field)
+    return new_headers
+
+
+def convert_field_to_name(field, fields_to_names):
+    return fields_to_names[field]
+
+
+def process(csv_file, do_file, dct_file, verbose=False):
     fields_to_types = process_types(dct_file)
     (fields_to_names,
     fields_to_parent_fields,
-    field_to_values_to_label) = process_headers(do_file)
-    field_to_value_for_record = get_single_record(csv_file) 
-    for field in field_to_value_for_record:
-        if field == '': continue
-        value = field_to_value_for_record[field]
-        name = fields_to_names[field]
-        field_type = fields_to_types[field]
-        parent_field = \
-            get_parent_field(field, fields_to_parent_fields)
-        interpret_value = \
-            get_interpret_value(parent_field, value, field_to_values_to_label)
-        if only_interpretable is True:
-            if interpret_value == value:
-                continue
-        print("{: >10.10} {: <5.5} {: >50.50} {:<15.15}"
-            .format(field, field_type, name, interpret_value))
+    field_to_values_to_label) = process_metadata(do_file)
+    reader = csv.DictReader(open(csv_file, 'r'))
+    headers = reader.fieldnames
+    filtered_headers = filter_headers(
+        headers,
+        fields_to_types,
+        fields_to_parent_fields)
+    named_headers = [convert_field_to_name(
+        field, fields_to_names) for field in filtered_headers]
+    writer = csv.DictWriter(
+        open(csv_file + '-processed.csv', 'w+'),
+        fieldnames=named_headers)
+    writer.writeheader()
+    for row in tqdm(reader):
+        output_row = {}
+        for field in filtered_headers:
+            name = convert_field_to_name(field, fields_to_names)
+            value = row[field].strip()
+            parent_field = get_parent_field(
+                field, fields_to_parent_fields)
+            interpret_value = get_interpret_value(
+                parent_field, value, field_to_values_to_label)
+            output_row[name] = interpret_value
+            if verbose is True:
+                print("{: >10.10} {: >50.50} {:<15.15}"
+                    .format(field, name, interpret_value))
+        writer.writerow(output_row)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process data.')
     parser.add_argument('data_folder', help='data folder to load')
-    parser.add_argument('--interpretable', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
     args = parser.parse_args()
     try:
         csv_file = glob.glob(args.data_folder + '/*.CSV')[0]
@@ -122,4 +137,4 @@ if __name__ == '__main__':
         dct_file = glob.glob(args.data_folder + '/*.DCT')[0]
     except:
         print("Make sure folder has a CSV, DO, and DCT file")
-    process(csv_file, do_file, dct_file, only_interpretable=args.interpretable)
+    process(csv_file, do_file, dct_file, args.verbose)
