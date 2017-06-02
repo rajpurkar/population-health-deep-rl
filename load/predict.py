7b093a25fd41624105d8524a5276414cb8a2dc7c
Rajpurkar <pranav.rajpurkar@gmail.com>
2017-06-02 15:27:19 -0700

    Make predict work

--- load/predict.py
+++ load/predict.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 import argparse
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report, f1_score
+from sklearn.model_selection import cross_val_score
 
-
-def predict(file, model='decision_treec'):
+def predict(file, model='endemicity_predict'):
     df = pd.read_csv(file, low_memory=False)
     train, test = train_test_split(df, test_size = 0.2)
     y_column = 'Final result of malaria from blood smear test'
@@ -23,23 +24,39 @@ def predict(file, model='decision_treec'):
     x_test = test[x_columns].values
     y_test = test[y_column].values
 
-    x = x_train
-    y_true = y_train
+    le = preprocessing.LabelEncoder()
+    y_train = le.fit_transform(y_train)
+    y_test = le.fit_transform(y_test)
+
+    x = x_test
+    y = y_test
+
     if model == 'all_no':
-        y_predict = ['Negative'] * len(x)
+        y_predict = [0] * len(x)
     elif model == 'endemicity_predict':
-        x_endem = x[:, name_to_col_index['Malaria endemicity']]
-        y_predict = np.array(['Negative'] * len(x))
-        flips = np.random.choice([0, 1], size=(len(x),), p=[0.625, 0.375])
-        y_predict[np.logical_and(x_endem == 'Lake endemic', flips == 1)] = 'Positive'
-        # todo: logistic regression rather than weird random assignment
+        from sklearn import tree
+
+        x_endem = x_train[:, name_to_col_index['Malaria endemicity']]
+        x_train = pd.get_dummies(x_endem).values
+
+        x = x[:, name_to_col_index['Malaria endemicity']]
+        x = pd.get_dummies(x).values
+
+        clf = tree.DecisionTreeClassifier()
+        clf.fit(x_train, y_train)
+
+        y_predict = clf.predict(x)
     elif model == 'decision_tree':
         ## todo: complete
         from sklearn import tree
         clf = tree.DecisionTreeClassifier()
         clf.fit(x_train, y_train)
         y_predict = clf.predict(x)
-    print(accuracy_score(y_true, y_predict))
+    print(classification_report(
+        y, y_predict))
+    print(accuracy_score(y, y_predict))
+    print(f1_score(
+        y, y_predict, average='binary'))
 
 
 if __name__ == '__main__':
