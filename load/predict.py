10efd3e6a1eca98b076d2e25ad71d781d8872dd9
Rajpurkar <pranav.rajpurkar@gmail.com>
2017-06-05 15:49:09 -0700

    Add arg for data out format

--- load/predict.py
+++ load/predict.py
@@ -18,19 +18,19 @@ def findsubsets(S,m):
     return set(itertools.combinations(S, m))
 
 
-def get_X_cols(df, names):
+def get_X_cols(df, names, flatten=True):
     X = df[names]
-    cols = X.columns
     feature_names = list(X.columns)
+    max_classes = 0
     for col in X:
         label_enc = LabelEncoder()
         X[col] = label_enc.fit_transform(X[col])
+        max_classes = max(len(label_enc.classes_), max_classes)
 
-    enc = OneHotEncoder(n_values=20, sparse=False)
+    enc = OneHotEncoder(n_values=max_classes, sparse=False)
     X = enc.fit_transform(X)
-
-    X = X.reshape((X.shape[0], len(cols), 1, -1))
-    print(X.shape)
+    if flatten is False:
+        X = X.reshape((X.shape[0], len(feature_names), 1, -1))
 
     return X, feature_names
 
@@ -45,7 +45,7 @@ def predict(file, model='endemicity_predict'):
     df = pd.read_csv(file, low_memory=False)
     y_column_name = 'Final result of malaria from blood smear test'
     columns = list(df.columns)
-    for num_features in range(len(columns), len(columns) + 1):
+    for num_features in range(len(columns) + 1, 0, -1):
         print(num_features, "features")
         for cols in tqdm(findsubsets(columns, num_features)):
             cols = list(cols)
@@ -53,12 +53,11 @@ def predict(file, model='endemicity_predict'):
             cols = filter(lambda col: not any(phrase.lower() in col.lower() for phrase in ignore_phrase_columns), cols)
             X, feature_names = get_X_cols(df, cols)
             y = get_Y_col(df, y_column_name)
-            # clf = sklearn.neural_network.MLPClassifier()
             clf = sklearn.linear_model.LogisticRegression()
-            #clf = sklearn.linear_model.Lasso()
+            # clf = sklearn.neural_network.MLPClassifier()
+            # clf = sklearn.linear_model.Lasso()
             predictions = cross_val_predict(clf, X, y, n_jobs=-1, verbose=1)
             score = sklearn.metrics.precision_recall_fscore_support(y, predictions, average='binary')
-            #score = sklearn.metrics.f1_score(y, predictions, average='binary')
             pp.pprint(cols)
             pp.pprint(score)
 
