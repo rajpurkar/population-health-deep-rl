9c29ecbbbf74be2676d8c7d2f4b0672c88297cbb
psvinaya <vinaya.polamreddi@gmail.com>
2017-06-05 16:47:06 -0700

    clean up network code

--- load/sl.py
+++ load/sl.py
@@ -6,15 +6,13 @@ from predict import *
 
 # Parameters
 learning_rate = 0.001
-training_epochs = 15
+training_epochs = 1
 batch_size = 100
 display_step = 1
 total_batch = 10
+eval_step = 1
 
 # Network Parameters
-n_hidden_1 = 256 # 1st layer number of features
-n_hidden_2 = 256 # 2nd layer number of features
-n_input = 295
 n_classes = 2
 
 def get_fake_dataset(batch_size, width, height, depth):
@@ -47,19 +45,6 @@ def get_next_batch(input_X, input_y, i, batch_size):
     return batch_X, batch_y
 
 # Create model
-def network(x, weights, biases):
-    # Hidden layer with RELU activation
-    x = layers.flatten(x)
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-    # Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-    # Output layer with linear activation
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-    return out_layer
-
-# Create model
 def cnn_network(x):
     out = x
     out = layers.convolution2d(out, num_outputs=10, kernel_size=[1,1], activation_fn=tf.nn.relu, stride=4)
@@ -76,62 +61,63 @@ def add_placeholder(width, height, depth):
     y = tf.placeholder("int64", [None, ])
     return x, y
 
-# Store layers weight & bias
-weights = {
-    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
-}
-biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_classes]))
-}
+def build(input_dim):
+    width = input_dim[1]
+    height = input_dim[2]
+    depth = input_dim[3]
 
-#input_X, input_y = get_fake_dataset(batch_size*total_batch, 103, 1, 400)
+    #get placeholders:
+    x, y = add_placeholder(width, height, depth)
 
-input_X, input_y = get_3d_data()
-print (input_X.shape)
-width = input_X.shape[1]
-height = input_X.shape[2]
-depth = input_X.shape[3]
+    # Construct model
+    pred = cnn_network(x)
 
-#get placeholders:
-x, y = add_placeholder(width, height, depth)
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-# Construct model
-pred = cnn_network(x)
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+    return x, y, pred, cost, optimizer, init
 
-# Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+def run(x, y, pred, cost, optimizer, init):
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
 
-# Initializing the variables
-init = tf.global_variables_initializer()
+        # Training cycle
+        for epoch in range(training_epochs):
+            avg_cost = 0.
+            # Loop over all batches
+            for i in range(total_batch - 1):
+                batch_x, batch_y = get_next_batch(input_X, input_y, i, batch_size)
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                              y: batch_y})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                print("Epoch:", '%04d' % (epoch+1), "cost=", \
+                    "{:.9f}".format(avg_cost))
+            if epoch % eval_step == 0:
+                # Test model
+                predictions = tf.argmax(pred, 1)
+                evalx, evaly = get_next_batch(input_X, input_y, total_batch - 1, batch_size)
+                output = predictions.eval({x: evalx, y: evaly})
+                score = sklearn.metrics.precision_recall_fscore_support(evaly, output, average='binary')
+                print(score)
+        print("Optimization Finished!")
 
-# Launch the graph
-with tf.Session() as sess:
-    sess.run(init)
-
-    # Training cycle
-    for epoch in range(training_epochs):
-        avg_cost = 0.
-        # Loop over all batches
-        for i in range(total_batch - 1):
-            batch_x, batch_y = get_next_batch(input_X, input_y, i, batch_size)
-            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
-                                                          y: batch_y})
-            # Compute average loss
-            avg_cost += c / total_batch
-        # Display logs per epoch step
-        if epoch % display_step == 0:
-            print("Epoch:", '%04d' % (epoch+1), "cost=", \
-                "{:.9f}".format(avg_cost))
-    print("Optimization Finished!")
-
-    # Test model
-    predictions = tf.argmax(pred, 1)
-    evalx, evaly = get_next_batch(input_X, input_y, total_batch - 1, batch_size)
-    output = predictions.eval({x: evalx, y: evaly})
-    score = sklearn.metrics.precision_recall_fscore_support(evaly, output, average='binary')
-    print(score)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='File to predict')
+    args = parser.parse_args()
+    predict_from_data(args.file)
+    if file is not None:
+        input_X, input_y = get_3d_data(file)
+    else:
+        input_X, input_y = get_3d_data()
+    print ("Inputx shape: " , input_X.shape)
+    print ("Inputy shape: " , input_y.shape)
+    x, y, pred, cost, optimizer, init = build(input_X.shape)
+    run(x, y, pred, cost, optimizer, init)
