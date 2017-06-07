56165e75f94adec3e4098167ac196c2a31122998
Anusha Balakrishnan <anusha.bala28@gmail.com>
2017-06-06 19:42:52 -0700

    SL agent uses env to get state and make predictions

--- load/sl.py
+++ load/sl.py
@@ -4,12 +4,13 @@ import tensorflow.contrib.layers as layers
 from predict import *
 from utils.survey_env import SurveyEnv
 from configs.survey_env import config as SurveyEnvConfig
+from utils.dataset import Dataset
 
 
 class Config():
     def __init__(self, epochs=50, batch_size=100, n_classes=2,
                  learning_rate=5e-4, reg=1e-1, display_step=1, eval_step=1,
-                 weighted_loss=False):
+                 weighted_loss=False, num_train_examples=6000, num_test_examples=100):
         self.epochs = epochs
         self.batch_size = batch_size
         self.n_classes = n_classes
@@ -18,6 +19,8 @@ class Config():
         self.display_step = display_step
         self.eval_step = eval_step
         self.weighted_loss = weighted_loss
+        self.num_train_examples = num_train_examples
+        self.num_test_examples = num_test_examples
 
 
 def get_fake_dataset(batch_size, width, height, depth, n_classes=2):
@@ -25,17 +28,6 @@ def get_fake_dataset(batch_size, width, height, depth, n_classes=2):
     batch_y = np.random.randint(n_classes, size=(batch_size))
     batch_y = np.array(batch_y)
     return batch_x, batch_y
-
-
-def get_next_batch(env, batch_size):
-    batch_X = []
-    batch_y = []
-    for k in xrange(0, batch_size):
-        ex_x, ex_y = env.reset(get_y=True)
-        batch_X.append(ex_x)
-        batch_y.append(ex_y)
-
-    return np.asarray(batch_X), np.asarray(batch_y)
 
 
 def cnn_network(config, x):
@@ -55,9 +47,9 @@ def add_placeholder(width, height, depth):
 
 
 def build(config, input_dim):
-    width = input_dim[1]
-    height = input_dim[2]
-    depth = input_dim[3]
+    width = input_dim[0]
+    height = input_dim[1]
+    depth = input_dim[2]
 
     #get placeholders:
     x, y = add_placeholder(width, height, depth)
@@ -79,29 +71,61 @@ def build(config, input_dim):
     return x, y, pred, cost, optimizer, init
 
 
-def get_rewards(env, gt_y, output):
+def get_state(env, split='train'):
     total_reward = 0.
-    for name in feature_names:
-        total_reward += env.reward_config.get_reward(name)
+    state = env.reset(split=split)
 
-    rewards = np.ones_like(output) * total_reward
-    rewards[output == gt_y] = env.reward_config.correctAnswerReward
-    rewards[output != gt_y] = env.reward_config.wrongAnswerReward
+    for f in xrange(0, env.feature_length):
+        state, r, done = env.step(f)
+        total_reward += r
+        assert not done
 
-    return np.mean(rewards)
+    return state, total_reward
 
 
-def run(train_env, test_env, x, y, pred, cost, optimizer, init, config=None, overfit=True):
+def get_prediction_reward(env, prediction):
+    _, r, done = env.step(env.feature_length + prediction[0])
+    assert done
+    return r
+
+
+def run_epoch(env, x, y, pred, cost, optimizer, sess, num_examples, split='train'):
+    avg_cost = 0.
+    avg_reward = 0.
+    # Loop over all batches
+
+    for i in range(num_examples):
+        example_X, reward = get_state(env, split=split)
+        example_X = np.expand_dims(example_X, axis=0)
+        q_reward = reward
+        train_pred = sess.run([pred], feed_dict={x: example_X})
+        train_pred = train_pred[0]
+        predictions = tf.arg_max(tf.nn.softmax(train_pred), dimension=1)
+        output = predictions.eval()
+        pred_reward = get_prediction_reward(env, output)
+        if pred_reward == env.config.correctAnswerReward:
+            example_y = output
+        else:
+            example_y = 1 - output
+
+        if split == 'train':
+            _, c = sess.run([optimizer, cost], feed_dict={x: example_X, y: example_y})
+            avg_cost += c
+
+        # Compute average loss
+        avg_reward += q_reward + pred_reward
+
+    if split == 'train':
+        avg_cost /= num_examples
+
+    avg_reward /= num_examples
+
+    return avg_cost, avg_reward
+
+
+def run(env, x, y, pred, cost, optimizer, init, config=None, overfit=True):
     if config is None:
         config = Config()
-
-    if overfit:
-        print("Trying to overfit..")
-        total_batch = 20
-    else:
-        total_batch = train_X.shape[0] / config.batch_size
-
-    print("Number of training batches: {:d}".format(total_batch))
 
     # Launch the graph
     with tf.Session() as sess:
@@ -109,44 +133,20 @@ def run(train_env, test_env, x, y, pred, cost, optimizer, init, config=None, ove
 
         # Training cycle
         for epoch in range(config.epochs):
-            avg_cost = 0.
-            # Loop over all batches
-            all_train_preds = None
-            all_train_y = None
 
-            for i in range(total_batch):
-                batch_x, batch_y = get_next_batch(train_env, config.batch_size)
-                train_pred, _, c = sess.run([pred, optimizer, cost], feed_dict={x: batch_x,
-                                                                                y: batch_y})
-                predictions = tf.arg_max(tf.nn.softmax(train_pred), dimension=1)
-                output = predictions.eval()
-                if all_train_preds is None:
-                    all_train_preds = output
-                else:
-                    all_train_preds = np.concatenate([all_train_preds, output], axis=0)
-
-                if all_train_y is None:
-                    all_train_y = batch_y
-                else:
-                    all_train_y = np.concatenate([all_train_y, batch_y], axis=0)
-                # Compute average loss
-                avg_cost += c / total_batch
-
+            avg_train_cost, avg_train_reward = run_epoch(env, x, y, pred, cost, optimizer,
+                                                         sess, config.num_train_examples)
             # Display logs per epoch step
             if (epoch+1) % config.display_step == 0:
-                print(len(all_train_y))
-                print(len(all_train_preds))
-                avg_reward = get_rewards(train_env, all_train_y, all_train_preds)
-                print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_cost))
-                print("Average train reward: {:.2f} ".format(avg_reward))
+                print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_train_cost))
+                print("Average train reward: {:.2f} ".format(avg_train_reward))
 
             if (epoch+1) % config.eval_step == 0:
                 # Test model
-                test_batch_X, test_batch_y = get_next_batch(test_env, survey_config.num_episodes_test)
-                predictions = tf.arg_max(tf.nn.softmax(pred), dimension=1)
-                output = predictions.eval({x: test_batch_X, y: test_batch_y})
-                avg_reward = get_rewards(test_env, test_batch_y, output)
-                print("Average test reward: {:.2f}".format(avg_reward))
+                _, avg_test_reward = run_epoch(env, x, y, pred, cost, optimizer, sess,
+                                                           config.num_test_examples, split='test')
+                print("Average test reward: {:.2f}".format(avg_test_reward))
+
         print("Optimization Finished!")
 
 if __name__ == '__main__':
@@ -155,30 +155,17 @@ if __name__ == '__main__':
     parser.add_argument('--overfit', action='store_true', help='If specified, tries to overfit to a single batch of '
                                                                'training data')
     args = parser.parse_args()
-    input_X, input_y, feature_names = get_X_Y_from_data(args.file)
-    print ("Inputx shape: ", input_X.shape)
-    print ("Inputy shape: ", input_y.shape)
 
-    config = Config()
+    config = Config(epochs=1)
     if args.overfit:
         config.reg = 0
         config.epochs = 250
         config.eval_step = 10000  # don't display stats on test set if overfitting
 
     # input_weights = [k+1 for k in input_y]
-    if config.weighted_loss:
-        neg_count = float(len([k for k in input_y if k == 0]))
-        pos_count = float(len([k for k in input_y if k == 1]))
-        input_weights = [neg_count/pos_count if k == 1. else 1. for k in input_y]
-    else:
-        input_weights = [1.0] * len(input_y)
-
-    train_X, train_y, test_X, test_y = split_data(0.8, input_X, input_y)
-    print(train_X.shape)
-    print(train_y.shape)
-
+    sampler = Dataset(args.file)
     survey_config = SurveyEnvConfig()
-    train_env = SurveyEnv(train_X, train_y, feature_names, survey_config)
-    test_env = SurveyEnv(test_X, test_y, feature_names, survey_config)
-    x, y, pred, cost, optimizer, init = build(config, input_X.shape)
-    run(train_env, test_env, x, y, pred, cost, optimizer, init, config=config, overfit=args.overfit)
+    env = SurveyEnv(survey_config, sampler)
+    x, y, pred, cost, optimizer, init = build(config, sampler.state_shape)
+    print(sampler.state_shape)
+    run(env, x, y, pred, cost, optimizer, init, config=config, overfit=args.overfit)
